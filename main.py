cdcf4540ece16987731c4209a0903e27f5b3b3d3
VKgitCod <itcreatorvak@gmail.com>
2024-07-05 23:02:37 +0300

    add hw

--- main.py
+++ main.py
@@ -18,12 +18,22 @@ target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH-target_width)
 target_y = random.randint(0, SCREEN_HEIGHT-target_height)
 
+# Скорость движения цели
+target_speed_x = 13
+target_speed_y = 13
+
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+hits = 0   # Переменная для хранения количества попаданий
+font = pygame.font.Font(None, 36)   # Инициализация шрифта
 
 running = True        # Переменная для переключения состояния цикла игры
 
 while running:
+
     screen.fill(color)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -32,8 +42,25 @@ while running:
             if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                hits += 1
+            else:
+                hits -= 1
+
+        # Обновление позиции цели
+        target_x += target_speed_x
+        target_y += target_speed_y
+
+        # Проверка границ экрана и изменение направления движения
+        if target_x <= 0 or target_x >= SCREEN_WIDTH - target_width:
+            target_speed_x = -target_speed_x
+        if target_y <= 0 or target_y >= SCREEN_HEIGHT - target_height:
+            target_speed_y = -target_speed_y
 
     screen.blit(target_img, (target_x, target_y))
+
+    text_surface = font.render(f"В цель: {hits}", True, (255, 255, 255))   # Создание текстовой поверхности
+    screen.blit(text_surface, (600, 10))   # Отображение текста на экране в верхнем левом углу
+
     pygame.display.update()
 
 
