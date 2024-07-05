52377fdaaf0f58069f9f2b68d9862c386facca30
VKgitCod <itcreatorvak@gmail.com>
2024-07-05 20:09:09 +0300

    add code base

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+
+running = True        # Переменная для переключения состояния цикла игры
+
+while running:
+    pass              # для пропуска запуска цикла
+
+pygame.quit()
